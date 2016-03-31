2df583f4b19939f966bcf3c1f584399fc4604f89
Dimitrios Kouzis-Loukas <lookfwd@gmail.com>
2016-03-31 18:54:39 +0100

    Add collection on the test

--- test_dj_mongo_database_url.py
+++ test_dj_mongo_database_url.py
@@ -1,44 +1,48 @@
 # -*- coding: utf-8 -*-
-#!/usr/bin/env python
 
 import os
-import unittest
 
-import dj_mongo_database_url
-
-
-MONGOLAB_URL = "mongodb://username:password@ds029297.mongolab.com:29297/my_db"
-
-
-class DatabaseTestSuite(unittest.TestCase):
-
-    def test_truth(self):
-        assert True
-
-    def test_mongodb_parsing(self):
-        url = 'mongodb://username:password@ds029297.mongolab.com:29297/my_db?my_option=true'
-        url = dj_mongo_database_url.parse(url)
-
-        assert url['NAME'] == 'my_db'
-        assert url['HOST'] == 'ds029297.mongolab.com'
-        assert url['USER'] == 'username'
-        assert url['PASSWORD'] == 'password'
-        assert url['PORT'] == 29297
-
-    def test_database_url(self):
-        a = dj_mongo_database_url.config()
-        assert not a
-
-        os.environ['MONGO_DATABASE_URL'] = 'mongodb://username:password@ds029297.mongolab.com:29297/my_db'
-
-        url = dj_mongo_database_url.config()
-
-        assert url['NAME'] == 'my_db'
-        assert url['HOST'] == 'ds029297.mongolab.com'
-        assert url['USER'] == 'username'
-        assert url['PASSWORD'] == 'password'
-        assert url['PORT'] == 29297
+try:
+	import urlparse
+except ImportError:
+	import urllib.parse as urlparse
 
 
-if __name__ == '__main__':
-    unittest.main()
+DEFAULT_ENV = 'MONGO_DATABASE_URL'
+
+def config(env=DEFAULT_ENV, default=None):
+	"""Returns configured DATABASE dictionary from MONGO_DATABASE_URL."""
+
+	config = {}
+
+	s = os.environ.get(env, default)
+
+	if s:
+		config = parse(s)
+
+	return config
+
+
+def parse(url):
+	"""Parses a database URL."""
+
+	config = {}
+
+	url = urlparse.urlparse(url)
+
+	# Remove query strings.
+	path = url.path[1:]
+	path = path.split('?', 2)[0]
+	path = path.split("/", 1)
+
+	# Update with environment configuration.
+	config.update({
+		'NAME': path[0],
+		'USER': url.username,
+		'PASSWORD': url.password,
+		'HOST': url.hostname,
+		'PORT': url.port,
+		'COLLECTION': path[1] if len(path) > 1 else None,
+	})
+
+	return config
